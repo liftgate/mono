1a0d03d8d0f6e09383e86f7ef39992de512f6bb4
Subham K. <me@growly.gg>
2024-12-17 19:14:58 -0500

    chore: some updates

--- src/test/kotlin/StateTests.kt
+++ src/test/kotlin/StateTests.kt
@@ -43,7 +43,11 @@ class StateTests : StateHolder()
         thread {
             while (!completion)
             {
-                allPeriodic()
+                runCatching {
+                    allPeriodic()
+                }.onFailure {
+                    it.printStackTrace()
+                }
                 Thread.sleep(50L)
             }
         }
